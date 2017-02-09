cbf26f1cd61b22aaa944186b2a73e5aa16124a04
chenxeed <chenxeed@gmail.com>
2017-02-10 01:39:23 +0800

    (fix) missing

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ Webpack2Polyfill.prototype.apply = function(compiler){
         "if(typeof window != \"undefined\") return window;",
         "if(typeof global != \"undefined\") return global;",
         "if(typeof self != \"undefined\") return self;",
-        "})()",
+        "})())",
         comments("Webpack2 Polyfill end")
       ]));
 
